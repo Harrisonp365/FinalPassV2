9838514357589959b704eb3f6ad9a552c98186dd
Harrisonp365 <harrisonpowell365@gmail.com>
2021-06-08 12:46:53 +1000

    init setup for secondary database for password storage

--- DbManager.h
+++ DbManager.h
@@ -12,14 +12,19 @@ public:
     ~DbManager();
     bool isOpen() const;
     bool createTable();
-    bool createPassTable();
+
+    //Signin/Login functions below
     bool addUser(const QString& username, const QString& password);
     bool removeUser(const QString& username, const QString& password);
     bool userExists(const QString& username, const QString& password);
-    int getUserId(const QString& username, const QString& password);
     bool usernameExists(const QString& username);
     void listAllUsers() const;
     bool removeAllUsers();
+    //Password storage functions below
+
+    bool createPassTable();
+    int getUserId(const QString& username, const QString& password);
+
 
 private:
     DbManager(const QString& path);
